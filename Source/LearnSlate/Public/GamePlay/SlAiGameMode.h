6466487310c0705bf5ef48b1d54a58da57c95308
hongilywang <hongilywang@sina.com>
2019-07-12 13:31:36 +0800

    为游戏场景，创建GameInstance, GameMode和GameHUD文件

---
+++ Source/LearnSlate/Public/GamePlay/SlAiGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlAiGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNSLATE_API ASlAiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LearnSlate/Public/UI/HUD/SlAiGameHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SlAiGameHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNSLATE_API ASlAiGameHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
