c75fe59db38ff4fb8badba2927eb282aab89c615
hongilywang <hongilywang@sina.com>
2019-08-07 13:31:10 +0800

    创建资源对象文件

---
+++ Source/LearnSlate/Public/Resource/SlAiResourceRock.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Resource/SlAiResourceObject.h"
+#include "SlAiResourceRock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNSLATE_API ASlAiResourceRock : public ASlAiResourceObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LearnSlate/Public/Resource/SlAiResourceTree.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Resource/SlAiResourceObject.h"
+#include "SlAiResourceTree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNSLATE_API ASlAiResourceTree : public ASlAiResourceObject
+{
+	GENERATED_BODY()
+	
+};
