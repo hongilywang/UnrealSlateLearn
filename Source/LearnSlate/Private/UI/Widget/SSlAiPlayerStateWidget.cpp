b9fccc74e56810405f1da1e8c7f9d7d58edd1251
hongilywang <hongilywang@sina.com>
2019-08-29 13:29:48 +0800

    玩家状态widget文件创建

---
+++ Source/LearnSlate/Private/UI/Widget/SSlAiPlayerStateWidget.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SSlAiPlayerStateWidget.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SSlAiPlayerStateWidget::Construct(const FArguments& InArgs)
+{
+	/*
+	ChildSlot
+	[
+		// Populate the widget
+	];
+	*/
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/LearnSlate/Public/UI/Widget/SSlAiPlayerStateWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class LEARNSLATE_API SSlAiPlayerStateWidget : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SSlAiPlayerStateWidget)
+	{}
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+};
