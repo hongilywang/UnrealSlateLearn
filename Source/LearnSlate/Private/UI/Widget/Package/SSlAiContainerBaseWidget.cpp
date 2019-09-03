967ce6e23139e9fd7e4544f14b442b9671bed52f
hongilywang <hongilywang@sina.com>
2019-09-03 13:31:29 +0800

    背包容器Base类。实现了一点点

--- Source/LearnSlate/Private/UI/Widget/Package/SSlAiContainerBaseWidget.cpp
+++ Source/LearnSlate/Private/UI/Widget/Package/SSlAiContainerBaseWidget.cpp
@@ -3,15 +3,21 @@
 
 #include "SSlAiContainerBaseWidget.h"
 #include "SlateOptMacros.h"
+#include "SlAiStyle.h"
+#include "SlAiGameWidgetStyle.h"
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 void SSlAiContainerBaseWidget::Construct(const FArguments& InArgs)
 {
-	/*
+	//获取GameStyle
+	GameStyle = &SlAiStyle::Get().GetWidgetStyle<FSlAiGameStyle>("BPSlAiGameStyle");
+	//获取工作号
+	WorkIndex = InArgs._WorkIndex;
+
 	ChildSlot
 	[
 		// Populate the widget
 	];
-	*/
+	
 }
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
--- Source/LearnSlate/Public/UI/Widget/Package/SSlAiContainerBaseWidget.h
+++ Source/LearnSlate/Public/UI/Widget/Package/SSlAiContainerBaseWidget.h
@@ -13,8 +13,19 @@ class LEARNSLATE_API SSlAiContainerBaseWidget : public SCompoundWidget
 public:
 	SLATE_BEGIN_ARGS(SSlAiContainerBaseWidget)
 	{}
+
+	SLATE_ATTRIBUTE(int, WorkIndex)
+
 	SLATE_END_ARGS()
 
 	/** Constructs this widget with InArgs */
 	void Construct(const FArguments& InArgs);
+
+protected:
+
+	//获取GameStyle
+	const struct FSlAiGameStyle* GameStyle;
+
+	//工作序号，用于标定在背包中的位置作用
+	TAttribute<int> WorkIndex;
 };
