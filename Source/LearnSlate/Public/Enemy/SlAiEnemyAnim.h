e4c9306492a84bfc90d4c711fac827f3d7d9209f
hongilywang <hongilywang@sina.com>
2019-09-24 13:33:16 +0800

    创建Enemy相关的c++类

---
+++ Source/LearnSlate/Public/Enemy/SlAiEnemyAnim.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlAiEnemyAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNSLATE_API USlAiEnemyAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LearnSlate/Public/EnemyTool/SlAiEnemySheild.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyTool/SlAiEnemyTool.h"
+#include "SlAiEnemySheild.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNSLATE_API ASlAiEnemySheild : public ASlAiEnemyTool
+{
+	GENERATED_BODY()
+	
+};
